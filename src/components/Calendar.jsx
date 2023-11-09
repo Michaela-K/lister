08a0daf6864116c591e9b1399c1bde1afeba0f0b
Michaela-K <michaelakodes@gmail.com>
2023-11-09 09:41:22 -0500

    add styling to calendar pane

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { CalendarDate, CaretUp } from 'react-bootstrap-icons'
 
 const Calendar = () => {
+  const calendarItems = ['today', 'next 7 days', 'all days']
+  
   return (
-    <div>Calendar</div>
+    <div className='calendar'>
+      <div className="header">
+        <div className="title">
+          <CalendarDate size="18"/>
+           <p>Calendar</p>
+        </div>
+                {/* <animated.div
+                    style={spin}
+                    onClick={() => setShowMenu(!showMenu)}
+                    className="btns"
+                > */}
+                <div className="btns">
+                    <span>
+                        <CaretUp size="20" />
+                    </span>
+                </div>
+                {/* </animated.div> */}
+      </div>
+        {calendarItems.map( item => 
+          <div className="items" key={item} 
+          // onClick={() => setSelectedProject(item)}
+          >
+            {item}
+          </div>
+        )}
+    </div>
   )
 }
 
